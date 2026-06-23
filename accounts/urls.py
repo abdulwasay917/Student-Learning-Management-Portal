992eec6c28e9ef042e48e463b97c681eabe6f447
Abdul Wasay <abdulwasayrao917@gmail.com>
2026-06-23 17:41:43 +0500

    accounts view

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    CreateUserAPI,
+    LoginAPI,
+    LogoutAPI,
+    ForgotPasswordAPI,
+    ResetPasswordAPI
+)
+
+urlpatterns = [
+    path('create-user/', CreateUserAPI.as_view()),
+    path('login/', LoginAPI.as_view()),
+    path('logout/', LogoutAPI.as_view()),
+    path('forgot-password/', ForgotPasswordAPI.as_view()),
+    path('reset-password/<uidb64>/<token>/', ResetPasswordAPI.as_view()),
+]
